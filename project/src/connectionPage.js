7ed5b4e33a32ae08b6a816e32fd583191f9d5b9a
Jahnavichinta <jahnavichinta6@gmail.com>
2024-07-23 12:03:55 +0530

    TestCommit

--- project/src/connectionPage.js
+++ project/src/connectionPage.js
@@ -8,7 +8,7 @@ function ConnectionPage() {
             <Cart path={'boy2.jpeg'} name={'Annie'} description={'Manager'}/>
             <Cart path={'boy1.jpeg'} name={'Steve Jobs'} description={'Software Developer'} />
             <Cart path={'girl1.jpeg'} name={'Annie'} description={'Manager'}/>
-            <Cart path={'boy2.jpeg'} name={'peter Paul'} description={'Software Developer'} />
+            <Cart path={'boy2.jpeg'} name={'peter'} description={'Software Developer'} />
             <Cart path={'girl2.jpeg'} name={'Annie'} description={'Manager'}/>
             <Cart path={'boy1.jpeg'} name={'Steve'} description={'Software Developer'} />
             <Cart path={'girl1.jpeg'} name={'Annie'} description={'Manager'}/>
