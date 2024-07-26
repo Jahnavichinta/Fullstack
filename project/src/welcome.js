2e23114117140fd0e4b7a64589122e9b82b49428
Jahnavichinta <jahnavichinta6@gmail.com>
2024-07-26 23:12:30 +0530

    COMMIT658tbxq9u

--- project/src/welcome.js
+++ project/src/welcome.js
@@ -4,8 +4,9 @@ function Welcome() {
     return (
         <div>
             <Navigation/>
-            <div>
-                <h2>Welcome</h2>
+            <div style={{margin:'auto'}}>
+                <center><h2>Welcome</h2></center>
+                <center><h2>Happy Shopping!!!</h2></center>
             </div>
         </div>
     )
